15564d44b829b7f95c934ee8a830fc2594055ba5
Srdjan Pejic <srdjan.pejic@xcentric.it>
2022-01-13 09:30:22 +0100

    BIH-161 methods cleanup

--- src/Actions/Crud/CrudActionTrait.php
+++ src/Actions/Crud/CrudActionTrait.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Support\Str;
 
 trait CrudActionTrait
@@ -18,13 +19,13 @@ trait CrudActionTrait
         $parsedData = [];
         $parsedRelationData = [];
         foreach ($data as $key => $item) {
-            $keyCamelCase = Str::camel((string) $key);
-            if ($model->isRelation($keyCamelCase)) {
-                if ($model->isFillable((string) $key) || $model->isFillable($keyCamelCase)) {
+            $key_camel = Str::camel($key);
+            if ($model->isRelation($key_camel)) {
+                $isFillable = !$model->isFillable($key) && $model->isFillable($key . '_id');
+                if ($isFillable) {
+                    $parsedData[$key . '_id'] = (!empty($item)  && is_array($item) && array_key_exists('id', $item)) ? $item['id'] : $item;;
+                } else {
                     $parsedRelationData[$key] = $item;
-                } elseif ($model->isFillable($key . '_id')) {
-                    $parsedData[$key . '_id'] = (!empty($item)
-                        && is_array($item) && array_key_exists('id', $item)) ? $item['id'] : $item;
                 }
             } else {
                 $parsedData[$key] = $item;
@@ -36,22 +37,20 @@ trait CrudActionTrait
     protected function fillRelationships(Model $model, array $data): void
     {
         foreach ($data as $key => $item) {
-            $keyCamelCase = Str::camel((string) $key);
-            if (
-                $model->isRelation($keyCamelCase) && ($model->isFillable((string) $key)
-                    || $model->isFillable($keyCamelCase))
-            ) {
-                $relationshipType = get_class($model->$keyCamelCase());
-                switch ($relationshipType) {
+            $key_camel = Str::camel($key);
+            if ($model->isRelation($key_camel)) {
+                $relationship_type = get_class($model->$key_camel());
+                switch ($relationship_type) {
                     case BelongsToMany::class:
-                        $this->syncBelongsToManyRelationship($model, $keyCamelCase, $item);
+                    case MorphToMany::class:
+                        $this->syncBelongsToManyRelationship($model, $key_camel, $item);
                         break;
                     case MorphMany::class:
                     case HasMany::class:
-                        $this->syncHasManyRelationship($model, $keyCamelCase, $item);
+                        $this->syncHasManyRelationship($model, $key_camel, $item);
                         break;
                     case BelongsTo::class:
-                        $this->syncBelongsToRelationship($model, $keyCamelCase, $item);
+                        $this->syncBelongsToRelationship($model, $key_camel, $item);
                         break;
                     default:
                         break;
@@ -84,24 +83,30 @@ trait CrudActionTrait
         $model->$relationshipName()->sync($presentIds);
     }
 
-    protected function syncHasManyRelationship(Model $model, string $relationshipName, array $data): void
+    protected function syncHasManyRelationship(Model $model, $relationship_name, array $data)
     {
+        $unSyncedSubModels = $model->$relationship_name()->pluck('id')->all();
+        $subModelClass = $model->$relationship_name()->getRelated();
         foreach ($data as $related) {
             $id = $related['id'] ?? null;
-            /** @var Model $subModel */
-            $subModel = $model->$relationshipName()->getRelated();
-            $subModel = $subModel->newModelQuery()->find($id);
             if (isset($related['DIRTY'])) {
-                if ($subModel) {
-                    /* @phpstan-ignore-next-line */
-                    $subModel->fill($related)->save();
-                    $model->$relationshipName()->save($subModel);
-                } else {
-                    $model->$relationshipName()->create($related);
-                }
-            } elseif ($id) {
-                $model->$relationshipName()->save($subModel);
+                /** @var Model $subModel */
+                $subModel = $subModelClass->newModelQuery()->find($id);
+                $subModel->fill($related)->save();
+                $model->$relationship_name()->save($subModel);
+            } else {
+                /** @var Model $subModel */
+                $subModel = $model->$relationship_name()->create($related);
             }
+
+            if (($index = array_search($subModel->id, $unSyncedSubModels)) !== false) {
+                unset($unSyncedSubModels[$index]);
+            }
+        }
+
+        foreach ($unSyncedSubModels as $unSyncedSubModel) {
+            $record = $model->$relationship_name()->where('id', '=', $unSyncedSubModel)->first();
+            $record->delete();
         }
     }
 
--- src/LaravelCrudController.php
+++ src/LaravelCrudController.php
@@ -6,11 +6,7 @@ use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -195,51 +191,6 @@ class LaravelCrudController extends BaseController
         return $this->createResource($model);
     }
 
-    private function resolveRelationFields(Model $model, array $data)
-    {
-        $parsedData = [];
-        $parsedRelationData = [];
-        foreach ($data as $key => $item) {
-            $key_camel = Str::camel($key);
-            if ($model->isRelation($key_camel)) {
-                $isFillable = !$model->isFillable($key) && $model->isFillable($key . '_id');
-                if ($isFillable) {
-                    $parsedData[$key . '_id'] = (!empty($item)  && is_array($item) && array_key_exists('id', $item)) ? $item['id'] : $item;;
-                } else {
-                    $parsedRelationData[$key] = $item;
-                }
-            } else {
-                $parsedData[$key] = $item;
-            }
-        }
-        return [$parsedData, $parsedRelationData];
-    }
-
-    private function fillRelationships(Model $model, array $data): void
-    {
-        foreach ($data as $key => $item) {
-            $key_camel = Str::camel($key);
-            if ($model->isRelation($key_camel)) {
-                $relationship_type = get_class($model->$key_camel());
-                switch ($relationship_type) {
-                    case BelongsToMany::class:
-                    case MorphToMany::class:
-                        $this->syncBelongsToManyRelationship($model, $key_camel, $item);
-                        break;
-                    case MorphMany::class:
-                    case HasMany::class:
-                        $this->syncHasManyRelationship($model, $key_camel, $item);
-                        break;
-                    case BelongsTo::class:
-                        $this->syncBelongsToRelationship($model, $key_camel, $item);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    }
-
     private function addRemoveRelationships(Model $model, string $relationField, array $item, bool $add = true): void
     {
         $key_camel = Str::camel($relationField);
@@ -253,25 +204,6 @@ class LaravelCrudController extends BaseController
                     break;
             }
         }
-    }
-
-    private function syncBelongsToManyRelationship(Model $model, $relationship_name, array $data)
-    {
-        $present_ids = [];
-        foreach ($data as $related) {
-            $id = $related['id'] ?? null;
-            $relation = $model->$relationship_name();
-            $present_ids[$id] = $this->getPivotColumnData($relation, $related['pivot'] ?? []);
-
-            if (isset($related['DIRTY'])) {
-                /** @var Model $subModel */
-                $subModel = $relation->getRelated();
-                $subModel = $subModel->newModelQuery()->find($related['id']) ?? $subModel;
-                $subModel->fill($related)->save();
-            }
-        }
-
-        $model->$relationship_name()->sync($present_ids);
     }
 
     private function appendDetachBelongsToManyRelationship(Model $model, $relationship_name, array $data, bool $append = true)
@@ -295,30 +227,18 @@ class LaravelCrudController extends BaseController
         }
     }
 
-    private function syncHasManyRelationship(Model $model, $relationship_name, array $data)
+    private function getPivotColumnData(BelongsToMany $relation, array $data): array
     {
-        $unSyncedSubModels = $model->$relationship_name()->pluck('id')->all();
-        $subModelClass = $model->$relationship_name()->getRelated();
-        foreach ($data as $related) {
-            $id = $related['id'] ?? null;
-            if (isset($related['DIRTY'])) {
-                /** @var Model $subModel */
-                $subModel = $subModelClass->newModelQuery()->find($id);
-                $subModel->fill($related)->save();
-                $model->$relationship_name()->save($subModel);
-            } else {
-                /** @var Model $subModel */
-                $subModel = $model->$relationship_name()->create($related);
-            }
-
-            if (($index = array_search($subModel->id, $unSyncedSubModels)) !== false) {
-                unset($unSyncedSubModels[$index]);
+        $pivotData = [];
+        foreach ($data as $key => $value) {
+            if (
+                in_array($key, $relation->getPivotColumns(), true)
+                && $key !== $relation->getParent()->getCreatedAtColumn()
+                && $key !== $relation->getParent()->getUpdatedAtColumn()
+            ) {
+                $pivotData[$key] = $value;
             }
         }
-
-        foreach ($unSyncedSubModels as $unSyncedSubModel) {
-            $record = $model->$relationship_name()->where('id', '=', $unSyncedSubModel)->first();
-            $record->delete();
-        }
+        return $pivotData;
     }
 }
