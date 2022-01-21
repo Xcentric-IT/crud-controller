0e7cdf25e6c5883bed1cda9ccea00131ce653f56
Srdjan Pejic <srdjan.pejic@xcentric.it>
2022-01-21 16:42:47 +0100

    BIH-185 trigger crud events from appropriate actions

--- src/Actions/Crud/Delete.php
+++ src/Actions/Crud/Delete.php
@@ -7,11 +7,17 @@ namespace XcentricItFoundation\LaravelCrudController\Actions\Crud;
 use XcentricItFoundation\LaravelCrudController\Actions\ActionPayloadInterface;
 use XcentricItFoundation\LaravelCrudController\Actions\ActionResponse;
 use XcentricItFoundation\LaravelCrudController\Actions\ExecutableActionResponseContract;
+use XcentricItFoundation\LaravelCrudController\Events\CrudModelAfterDelete;
+use XcentricItFoundation\LaravelCrudController\Events\CrudModelBeforeDelete;
 
 class Delete extends CrudAction
 {
     public function run(ActionPayloadInterface $actionPayload): ExecutableActionResponseContract
     {
-        return new ActionResponse($actionPayload->getModel()->delete());
+        CrudModelBeforeDelete::dispatch($actionPayload);
+        $actionResponse = new ActionResponse($actionPayload->getModel()->delete());
+        CrudModelAfterDelete::dispatch($actionPayload);
+
+        return $actionResponse;
     }
 }
