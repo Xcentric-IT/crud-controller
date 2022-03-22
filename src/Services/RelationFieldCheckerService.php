be7f9348211099f9facf283cb71d2cdc7b40791d
Mladen Kakuca <mladen.kakuca@xcentric.it>
2022-03-22 15:53:42 +0100

    hotfix for relation check, do not convert relations to camelCase

--- src/Services/RelationFieldCheckerService.php
+++ src/Services/RelationFieldCheckerService.php
@@ -10,21 +10,16 @@ class RelationFieldCheckerService
 {
     public function isRelationField(Model $model, string $field): bool
     {
-        return $model->isRelation($this->getRelationNameByField($field));
+        return $model->isRelation($field);
     }
 
     public function getRelationByField(Model $model, string $field): Relation
     {
-        return $model->{$this->getRelationNameByField($field)}();
+        return $model->{$field}();
     }
 
     public function getRelationClassByField(Model $model, string $field): string
     {
         return get_class($this->getRelationByField($model, $field));
-    }
-
-    public function getRelationNameByField(string $field): string
-    {
-        return Str::camel($field);
     }
 }
